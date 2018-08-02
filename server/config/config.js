5685824fd92b5c36f9ba6956bfe8faa447467b6e
Stefanel Stan <stefanel.stan@yahoo.com>
2018-08-02 20:27:30 +0100

    corrected typo

--- server/config/config.js
+++ server/config/config.js
@@ -9,5 +9,5 @@ if(env === 'development'){
 	process.env.MONGOBD_URI = 'mongodb://localhost:27017/TodoAppTest'
 } else if (env === 'production'){
     process.env.PORT = 3000;
-    process.end.MONGOBD_URI = 'mongodb://stef:stef1234@ds263571.mlab.com:63571/todoapp';
+    process.env.MONGOBD_URI = 'mongodb://stef:stef1234@ds263571.mlab.com:63571/todoapp';
 }
