97923fa81a24399c4ee1dc91b69796e142b660e0
Stefanel Stan <stefanel.stan@yahoo.com>
2018-08-02 20:35:45 +0100

    more changes to port

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,12 @@ var env = process.env.NODE_ENV || 'development';
 console.log('env*******', env);
 
 if(env === 'development'){
-	process.env.PORT = 3000;
+
 	process.env.MONGOBD_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test'){
-	process.env.PORT = 3000;
+
 	process.env.MONGOBD_URI = 'mongodb://localhost:27017/TodoAppTest'
 } else if (env === 'production'){
-    process.env.PORT = 3000;
+
     process.env.MONGOBD_URI = 'mongodb://stef:stef1234@ds263571.mlab.com:63571/todoapp';
 }
